c8d21f163708244faea759e90d5d179362a82764
Niclas Frohberg <niclas-frohberg@t-online.de>
2022-10-06 17:28:56 +0200

    add jumping

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -1,50 +1,14 @@
-using System.Collections;
 using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-    private int score = 0;
 
-    private bool lockedInput = false;
+    public float cameraSpeed = 0.1f;
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        score++;
-        PlayerPrefs.SetInt("Score", score);
         var newVector = this.transform.position;
-        newVector.z += 0.1f;
+        newVector.z += cameraSpeed;
         this.transform.position = newVector;
-
-        if (!lockedInput)
-        {
-            StartCoroutine(handleInput());
-            print(Time.fixedDeltaTime);
-            lockedInput = true;
-        }
-        
-    }
-
-
-    private IEnumerator handleInput()
-    {
-        // getestet mit Xbox Controller und funktioniert :
-        var horizontalValue = Input.GetAxis("Horizontal");
-
-        // move to right if possible
-        if (horizontalValue > 0 && transform.position.x != 1.5)
-        {
-            var newVector = transform.position;
-            newVector.x += 1;
-            transform.position = newVector;
-        } // move to left if possible
-        else if (horizontalValue < 0 && transform.position.x != -0.5)
-        {
-            var newVector = transform.position;
-            newVector.x -= 1;
-            transform.position = newVector;
-        }
-
-        yield return new WaitForSeconds(0.1f);
-        lockedInput = false;
     }
 }
