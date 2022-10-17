5e4d73af487d9bc74d9a2d3a8cee382408229658
FelixReschke <83491630+FelixReschke@users.noreply.github.com>
2022-10-17 10:23:17 +0200

    Skybox rotation

---
+++ Assets/Scripts/RotateSky.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSky : MonoBehaviour
+{
+    public float RotateSpeed = 1.2f;
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * RotateSpeed);
+    }
+}
